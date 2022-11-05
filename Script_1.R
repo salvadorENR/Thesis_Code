c9435e2a495d9d1537add746dae3cb4d9b80d86f
Salvador Enrique Rodríguez Hernández <salvadorestadistic@gmail.com>
2022-11-05 12:21:36 -0600

    Update Script_1.R

--- Script_1.R
+++ Script_1.R
@@ -8,3 +8,4 @@ chp10 <- chp10[!is.na(chp10$mathach)&!is.na(chp10$efficacy)&!is.na(chp10$public)
 chp10$csclimat <- chp10$sclimate-mean(chp10$sclimate, na.rm=TRUE)
 chp10$gceffic <- chp10$efficacy-mean(chp10$efficacy, na.rm=TRUE)
 attach(chp10)
+#************************** Preliminar results ******************************************************
